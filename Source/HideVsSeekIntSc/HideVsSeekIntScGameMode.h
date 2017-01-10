4552712e95845021404b40ac7a556365ebd85421
SudeepSingh91 <gamersudeep01@gmail.com>
2017-01-10 07:47:00 -0700

    Add files via upload
    Initial commit. Added files to source control.

---
+++ Source/HideVsSeekIntSc/HideVsSeekIntScGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "HideVsSeekIntScGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HIDEVSSEEKINTSC_API AHideVsSeekIntScGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
